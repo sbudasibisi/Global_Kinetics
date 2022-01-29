b8cc1e0d19461feb7810413ae8ac7587a76d4573
sibusisosibisi <sibusiso.sibisi@bidorbuy.co.za>
2022-01-29 02:05:32 +0200

    Initial Commit

---
+++ Global_Kinetics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Global_Kinetics
+//
+//  Created by Sibusiso Sibisi on 2022/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
